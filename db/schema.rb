91d27387c003381196b1fed47d2287ab114ea027
mofumofuchan <you_oki1@yahoo.co.jp>
2018-05-27 00:11:19 +0900

    generate user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_25_151301) do
+ActiveRecord::Schema.define(version: 2018_05_26_150758) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.integer "age"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "num_protein"
   end
 
 end
