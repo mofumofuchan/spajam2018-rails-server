4b6f09a9ef3642af8cdc8e4616b42cbc53f1e05b
mofumofuchan <you_oki1@yahoo.co.jp>
2018-05-27 01:09:41 +0900

    Remove a spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,9 +2,5 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   describe "validation" do
-    example "名前は3文字異常" do
-      user = User.new(name: "ab")
-      expect(user).not_to be_valid
-    end
   end
 end
