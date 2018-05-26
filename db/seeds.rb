299fbe1015d93ef08b17f48cf7b5a25093ebf709
dyuji@mac <d.yuji.form@gmail.com>
2018-05-27 07:49:40 +0900

    seedの追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,3 +10,27 @@ Objective.create(title: "今週2回ジムに行きます！",
                  done: false,
                  achievement: 10,
                  user: user1)
+
+Objective.create(title: "腹筋がんばります",
+                 start: 2018-05-21,
+                 end: 2018-05-28,
+                 menu: "腹筋/100回",
+                 done: false,
+                 achievement: 85,
+                 user: user2)
+
+Objective.create(title: "上腕二頭筋鍛えます",
+                 start: 2018-05-24,
+                 end: 1.week.since,
+                 menu: "腕立て伏せ/200回",
+                 done: false,
+                 achievement: 30,
+                 user: user3)
+
+Objective.create(title: "ジャンプ力のために鍛えます",
+                 start: 2018-05-26,
+                 end: 1.week.since,
+                 menu: "スクワット/50回",
+                 done: false,
+                 achievement: 60,
+                 user: user4)
