6272bca732c0abaef7a8eea10449ddeb0c28a595
mofumofuchan <you_oki1@yahoo.co.jp>
2018-05-27 04:02:23 +0900

    Fix seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,11 @@
-user1 = User.create(name: "小野順平", num_protein: 100)
+user1 = User.create(name: "小野純平", num_protein: 100)
 user2 = User.create(name: "沖本祐典", num_protein: 100)
 user3 = User.create(name: "道券裕二", num_protein: 100)
 user4 = User.create(name: "小島智樹", num_protein: 100)
 
 Objective.create(title: "今週2回ジムに行きます！",
-                 limit_date: 1.week.since,
-                 content: "一念発起してがんばります！お願いします！",
+                 start: Date.today,
+                 end: 1.week.since,
+                 menu: "ランニング/10km",
                  done: false,
                  user: user1)
