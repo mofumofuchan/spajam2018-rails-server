c8f6483c84dc7377c9d391598395d0abb7950167
mofumofuchan <you_oki1@yahoo.co.jp>
2018-05-27 12:57:12 +0900

    Modify seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,10 @@
 user1 = User.create(id: 1, name: "小野純平", num_protein: 100)
-user2 = User.create(id: 2, name: "小島智樹", num_protein: 100)
+user2 = User.create(id: 2, name: "小島智樹", num_protein: 0)
 user3 = User.create(id: 3, name: "道券裕二", num_protein: 100)
 user4 = User.create(id: 4, name: "沖本祐典", num_protein: 100)
 User.create(id: 5, name: "anonymous", num_protein: 100)
 
-Objective.create(title: "今週2回ジムに行きます！",
+Objective.create(title: "今週2回ジムに行く！",
                  start: Date.today,
                  end: 1.week.since,
                  menu: "ランニング-10km",
@@ -17,7 +17,7 @@ Objective.create(title: "腹筋がんばります",
                  end: "2018-05-28",
                  menu: "腹筋-100回",
                  done: false,
-                 achievement: 85,
+                 achievement: 0,
                  user: user2)
 
 Objective.create(title: "上腕二頭筋鍛えます",
@@ -28,7 +28,7 @@ Objective.create(title: "上腕二頭筋鍛えます",
                  achievement: 30,
                  user: user3)
 
-Objective.create(title: "ジャンプ力のために鍛えます",
+Objective.create(title: "ジャンプ力の向上",
                  start: "2018-05-26",
                  end: 1.week.since,
                  menu: "スクワット-50回",
